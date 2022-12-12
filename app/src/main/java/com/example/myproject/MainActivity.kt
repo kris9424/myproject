8111c2cdb853b71771a6921aab314b0430e83401
Kristina1 <kmyachina@yandex.ru>
2022-12-12 14:16:52 +0300

    task4

--- app/src/main/java/com/example/myproject/MainActivity.kt
+++ app/src/main/java/com/example/myproject/MainActivity.kt
@@ -5,27 +5,11 @@ import android.os.Bundle
 import androidx.fragment.app.FragmentManager
 
 class MainActivity : AppCompatActivity() {
-    lateinit var firstFragment: FirstFragment
-    lateinit var secondFragment: SecondFragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        firstFragment = FirstFragment.newInstance()
-        secondFragment = SecondFragment.newInstance()
-    }
-    fun switchToFirstFragment(){
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.container, firstFragment)
-            .commit()
+
     }
 
-    fun switchToSecondFragment(){
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.container, secondFragment)
-            .commit()
     }
-
-
 }
--- app/src/main/java/com/example/myproject/SecondFragment.kt
+++ app/src/main/java/com/example/myproject/SecondFragment.kt
@@ -6,20 +6,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 
 class SecondFragment : Fragment(R.layout.fragment_second) {
+    lateinit var controller: NavController
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        controller = findNavController()
         val button = view.findViewById<Button>(R.id.switchToFirstButton)
         button.setOnClickListener {
-            (requireActivity() as MainActivity).switchToFirstFragment()
+            controller.navigateUp()
         }
     }
-    companion object {
-        @JvmStatic
-        fun newInstance() =
-            SecondFragment()
 
-    }
 }
